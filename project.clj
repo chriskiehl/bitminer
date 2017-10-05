27608edad70449cd85bcd2d225779c525beb2cec
ckiehl <audionautic@gmail.com>
2017-10-04 22:36:54 -0700

    added Environ dep

--- project.clj
+++ project.clj
@@ -3,12 +3,14 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [digest "1.4.5"]
+  :dependencies [[digest "1.4.5"]
+                 [environ "1.1.0"]
+                 [org.clojure/clojure "1.8.0"]
                  [org.clojure/core.async "0.3.443"]
                  [org.clojure/data.json "0.2.6"]
                  [org.clojure/math.numeric-tower "0.0.4"]
                  [http-kit "2.2.0"]]
+  :plugins [[lein-environ "1.1.0"]]
   :main ^:skip-aot miner.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
